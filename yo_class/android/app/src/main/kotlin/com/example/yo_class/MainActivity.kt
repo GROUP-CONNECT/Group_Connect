81f63edba32832ceff63919cde903f4ea721d46d
Ssekyanzi-Joel <ssekyanzijoel0@gmail.com>
2022-10-11 16:47:05 +0300

    modifying

--- yo_class/android/app/src/main/kotlin/com/example/yo_class/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.yo_class
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ yo_class/lib/ui/base.dart
@@ -0,0 +1,69 @@
+import 'package:coursehub/ui/index.dart';
+import 'package:flutter/material.dart';
+
+class Base extends StatefulWidget {
+  @override
+  _BaseState createState() => _BaseState();
+}
+
+class _BaseState extends State<Base> {
+  int _current = 0;
+  final List<Widget> _screens = [
+    HomePage(),
+    ExplorePage(),
+    SingleCoursePage(),
+  ];
+
+  void _onTap(int index) {
+    try {
+      // Catch RangeError for missing pages
+      setState(() {
+        _current = index;
+      });
+    } catch (e) {
+      // page unimplemented
+      print('Missing Page');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_current],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onTap,
+        currentIndex: _current,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Color(0xff4769FF),
+              ),
+              label: 'home',
+              backgroundColor: Colors.white),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.launch,
+                color: Color(0xff4769FF),
+              ),
+              label: 'explore'),
+          // backgroundColor: Colors.white),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.grid_on,
+                color: Color(0xff4769FF),
+              ),
+              label: 'courses',
+              backgroundColor: Colors.white),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.person,
+                color: Color(0xff4769FF),
+              ),
+              label: 'profile',
+              backgroundColor: Colors.white),
+        ],
+      ),
+    );
+  }
+}
